fb869bbe5c1d7f73f515964d1aa346eb2a53cfed
Josh Zuech <josh.zuech@nextthought.com>
2020-04-08 16:09:17 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,92 @@
+import codecs
+from setuptools import setup, find_packages
+
+entry_points = {
+    "z3c.autoinclude.plugin": [
+        'target = nti.app.sites',
+    ],
+    'console_scripts': [
+    ],
+}
+
+TESTS_REQUIRE = [
+    'fudge',
+    'nti.dataserver[test]',
+    'nti.app.testing',
+    'nti.testing',
+    'zope.dottedname',
+    'zope.testrunner',
+]
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.app.site.license',
+    version=_read('version.txt').strip(),
+    author='Josh Zuech',
+    author_email='josh.zuech@nextthought.com',
+    description="NTI App Site License",
+    long_description=(
+        _read('README.rst')
+        + '\n\n'
+        + _read("CHANGES.rst")
+    ),
+    license='Apache',
+    keywords='app site license',
+    classifiers=[
+        'Framework :: Zope3',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    url="https://github.com/NextThought/nti.app.site.license",
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.app', 'nti.app.site'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'BTrees',
+        'nti.containers',
+        'nti.dublincore',
+        'nti.externalization',
+        'nti.mimetype',
+        'nti.property',
+        'nti.schema',
+        'nti.site.license',
+        'nti.wref',
+        'setuptools',
+        'six',
+        'ZODB',
+        'zope.annotation',
+        'zope.cachedescriptors',
+        'zope.component',
+        'zope.container',
+        'zope.event',
+        'zope.lifecycleevent',
+        'zope.location',
+        'zope.interface',
+        'zope.intid',
+        'zope.security',
+        'zope.traversing',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
